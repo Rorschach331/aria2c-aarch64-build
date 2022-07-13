caf19f806dc980e9d15c3f51f13dfc51f600863f
Rorschach <842007947@qq.com>
2022-07-13 11:19:25 +0800

    更新lib版本

--- libs.sh
+++ libs.sh
@@ -18,12 +18,12 @@
  fi
 
 ## DEPENDENCES ##
-ZLIB=http://sourceforge.net/projects/libpng/files/zlib/1.2.11/zlib-1.2.11.tar.gz
-OPENSSL=https://www.openssl.org/source/openssl-1.1.1p.tar.gz
-EXPAT=https://github.com/libexpat/libexpat/releases/download/R_2_4_1/expat-2.4.1.tar.bz2
-SQLITE3=https://sqlite.org/2021/sqlite-autoconf-3360000.tar.gz
-C_ARES=https://c-ares.haxx.se/download/c-ares-1.17.2.tar.gz
-SSH2=https://www.libssh2.org/download/libssh2-1.9.0.tar.gz
+ZLIB=https://sourceforge.net/projects/libpng/files/zlib/1.2.11/zlib-1.2.11.tar.gz
+OPENSSL=https://www.openssl.org/source/openssl-1.1.1q.tar.gz
+EXPAT=https://github.com/libexpat/libexpat/releases/download/R_2_4_8/expat-2.4.8.tar.bz2
+SQLITE3=https://sqlite.org/2022/sqlite-autoconf-3390000.tar.gz
+C_ARES=https://c-ares.org/download/c-ares-1.18.1.tar.gz
+SSH2=https://www.libssh2.org/download/libssh2-1.10.0.tar.gz
 
 ## CONFIG ##
 ARCH="armhf"
@@ -31,7 +31,7 @@ HOST="aarch64-linux-gnu"
 PREFIX="/opt/aria2-aarch64/build_libs"
 LOCAL_DIR="/opt/aria2-aarch64/build_libs"
 
-TOOL_BIN_DIR="/opt/aria2-aarch64/tools/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin"
+TOOL_BIN_DIR="/opt/aria2-aarch64/tools/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin"
 PATH=${TOOL_BIN_DIR}:$PATH
 
 CFLAGS="-march=armv8-a -mtune=cortex-a53"
@@ -59,8 +59,8 @@ cd $BUILD_DIRECTORY
  # expat build
   cd ..
   $DOWNLOADER $EXPAT
-  tar jxvf expat-2.4.1.tar.bz2
-  cd expat-2.4.1/
+  tar jxvf expat-2.4.8.tar.bz2
+  cd expat-2.4.8/
   PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ LD_LIBRARY_PATH=$PREFIX/lib/ CC=$HOST-gcc CXX=$HOST-g++ ./configure --host=$HOST --build=`dpkg-architecture -qDEB_BUILD_GNU_TYPE` --prefix=$PREFIX --enable-static=yes --enable-shared=no
   make
   make install
@@ -68,8 +68,8 @@ cd $BUILD_DIRECTORY
  # c-ares build
   cd ..
   $DOWNLOADER $C_ARES
-  tar zxvf c-ares-1.17.2.tar.gz
-  cd c-ares-1.17.2/
+  tar zxvf c-ares-1.18.1.tar.gz
+  cd c-ares-1.18.1/
   PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ LD_LIBRARY_PATH=$PREFIX/lib/ CC=$HOST-gcc CXX=$HOST-g++ ./configure --host=$HOST --build=`dpkg-architecture -qDEB_BUILD_GNU_TYPE` --prefix=$PREFIX --enable-static --disable-shared
   make
   make install
@@ -77,8 +77,8 @@ cd $BUILD_DIRECTORY
  # Openssl build
   cd ..
   $DOWNLOADER $OPENSSL
-  tar zxvf openssl-1.1.1p.tar.gz
-  cd openssl-1.1.1p/
+  tar zxvf openssl-1.1.1q.tar.gz
+  cd openssl-1.1.1q/
   PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ LD_LIBRARY_PATH=$PREFIX/lib/ CC=$HOST-gcc CXX=$HOST-g++  ./Configure linux-armv4 no-asm $CFLAGS --prefix=$PREFIX shared zlib zlib-dynamic -D_GNU_SOURCE -D_BSD_SOURCE --with-zlib-lib=$LOCAL_DIR/lib --with-zlib-include=$LOCAL_DIR/include
   make CC=$CC
   make CC=$CC install INSTALLTOP=$DEST OPENSSLDIR=$DEST/ssl
@@ -86,8 +86,8 @@ cd $BUILD_DIRECTORY
  # sqlite3
   cd ..
   $DOWNLOADER $SQLITE3
-  tar zxvf sqlite-autoconf-3360000.tar.gz
-  cd sqlite-autoconf-3360000/
+  tar zxvf sqlite-autoconf-3390000.tar.gz
+  cd sqlite-autoconf-3390000/
   PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ LD_LIBRARY_PATH=$PREFIX/lib/ CC=$HOST-gcc CXX=$HOST-g++ ./configure --host=$HOST --prefix=$PREFIX --enable-static --enable-shared --build=`dpkg-architecture -qDEB_BUILD_GNU_TYPE`
   make
   make install
@@ -95,8 +95,8 @@ cd $BUILD_DIRECTORY
  # libssh2
   cd ..
   $DOWNLOADER $SSH2
-  tar zxvf libssh2-1.9.0.tar.gz
-  cd libssh2-1.9.0/
+  tar zxvf libssh2-1.10.0.tar.gz
+  cd libssh2-1.10.0/
   rm -rf $PREFIX/lib/pkgconfig/libssh2.pc
   PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ LD_LIBRARY_PATH=$PREFIX/lib/ CC=$HOST-gcc CXX=$HOST-g++ AR=$HOST-ar RANLIB=$HOST-ranlib ./configure --host=$HOST --without-libgcrypt --with-openssl --without-wincng  --with-libssl-prefix=$PREFIX --prefix=$PREFIX --enable-static --disable-shared LDFLAGS="-L$LOCAL_DIR/openssl/lib" PKG_CONFIG_PATH="$LOCAL_DIR/openssl/lib/pkgconfig" CPPFLAGS="-I$DEST/openssl/include"
   make
