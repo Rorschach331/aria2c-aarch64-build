15b7b395e436101542b6548897a36d7f2b16c1f5
sdx <317338522@qq.com>
2026-02-11 08:52:08 +0800

    fix: 修复 OpenSSL 3.x Provider 加载失败报错并重构脚本参数化配置

--- libs.sh
+++ libs.sh
@@ -1,114 +1,133 @@
 #!/bin/bash
 
-# In this configuration, the following dependent libraries are compiled:
-#
-# * zlib
-# * c-ares
-# * expat
-# * sqlite3
-# * openSSL
-# * libssh2
+# ==========================================================
+# 统一的版本号配置
+# ==========================================================
+ZLIB_VER="1.2.11"
+OPENSSL_VER="3.4.0"
+EXPAT_VER="2.4.8"
+SQLITE_VER="3390000"
+C_ARES_VER="1.34.4"
+LIBSSH2_VER="1.11.1"
 
-#CHECK TOOL FOR DOWNLOAD
- aria2c --help > /dev/null
- if [ "$?" -eq 0 ] ; then
-   DOWNLOADER="aria2c --check-certificate=false"
- else
-   DOWNLOADER="wget -c --no-check-certificate"
- fi
+# ==========================================================
+# 下载链接配置
+# ==========================================================
+ZLIB_URL="https://sourceforge.net/projects/libpng/files/zlib/${ZLIB_VER}/zlib-${ZLIB_VER}.tar.gz"
+OPENSSL_URL="https://github.com/openssl/openssl/releases/download/openssl-${OPENSSL_VER}/openssl-${OPENSSL_VER}.tar.gz"
+EXPAT_URL="https://github.com/libexpat/libexpat/releases/download/R_${EXPAT_VER//./_}/expat-${EXPAT_VER}.tar.bz2"
+SQLITE3_URL="https://sqlite.org/2022/sqlite-autoconf-${SQLITE_VER}.tar.gz"
+C_ARE_URL="https://github.com/c-ares/c-ares/releases/download/v${C_ARES_VER}/c-ares-${C_ARES_VER}.tar.gz"
+SSH2_URL="https://libssh2.org/download/libssh2-${LIBSSH2_VER}.tar.gz"
 
-## DEPENDENCES ##
-ZLIB=https://sourceforge.net/projects/libpng/files/zlib/1.2.11/zlib-1.2.11.tar.gz
-OPENSSL=https://github.com/openssl/openssl/releases/download/openssl-3.4.0/openssl-3.4.0.tar.gz
-EXPAT=https://github.com/libexpat/libexpat/releases/download/R_2_4_8/expat-2.4.8.tar.bz2
-SQLITE3=https://sqlite.org/2022/sqlite-autoconf-3390000.tar.gz
-C_ARES=https://github.com/c-ares/c-ares/releases/download/v1.34.4/c-ares-1.34.4.tar.gz
-SSH2=https://libssh2.org/download/libssh2-1.11.1.tar.gz
-
-## CONFIG ##
-ARCH="armhf"
+# ==========================================================
+# 编译环境配置
+# ==========================================================
+ARCH="aarch64"
 HOST="aarch64-linux-gnu"
 PREFIX="/opt/aria2-aarch64/build_libs"
 LOCAL_DIR="/opt/aria2-aarch64/build_libs"
+DEST="/opt/aria2-aarch64/build_libs"
+BUILD_DIRECTORY="/tmp"
 
 TOOL_BIN_DIR="/opt/aria2-aarch64/tools/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin"
-PATH=${TOOL_BIN_DIR}:$PATH
+export PATH=${TOOL_BIN_DIR}:$PATH
 
 CFLAGS="-march=armv8-a -mtune=cortex-a53"
-DEST="/opt/aria2-aarch64/build_libs"
-CC=$HOST-gcc
-CXX=$HOST-g++
+CXXFLAGS=$CFLAGS
 LDFLAGS="-L$DEST/lib"
 CPPFLAGS="-I$DEST/include"
-CXXFLAGS=$CFLAGS
-MAKE="make -j`nproc`"
-CONFIGURE="./configure --prefix=${LOCAL_DIR} --host=$HOST"
-BUILD_DIRECTORY=/tmp/
 
-## BUILD ##
+CC=$HOST-gcc
+CXX=$HOST-g++
+STRIP=$HOST-strip
+RANLIB=$HOST-ranlib
+AR=$HOST-ar
+LD=$HOST-ld
+
+MAKE="make -j`nproc`"
+
+# ==========================================================
+# 工具检查
+# ==========================================================
+aria2c --help > /dev/null
+if [ "$?" -eq 0 ] ; then
+  DOWNLOADER="aria2c --check-certificate=false"
+else
+  DOWNLOADER="wget -c --no-check-certificate"
+fi
+
+# ==========================================================
+# 编译流程
+# ==========================================================
 cd $BUILD_DIRECTORY
-#
- # zlib build
-  $DOWNLOADER $ZLIB
-  tar zxvf zlib-1.2.11.tar.gz
-  cd zlib-1.2.11/
-  PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ LD_LIBRARY_PATH=$PREFIX/lib/ CC=$HOST-gcc STRIP=$HOST-strip RANLIB=$HOST-ranlib CXX=$HOST-g++ AR=$HOST-ar LD=$HOST-ld ./configure --prefix=$PREFIX --static
-  make
-  make install
-#
- # expat build
-  cd ..
-  $DOWNLOADER $EXPAT
-  tar jxvf expat-2.4.8.tar.bz2
-  cd expat-2.4.8/
-  PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ LD_LIBRARY_PATH=$PREFIX/lib/ CC=$HOST-gcc CXX=$HOST-g++ ./configure --host=$HOST --build=`dpkg-architecture -qDEB_BUILD_GNU_TYPE` --prefix=$PREFIX --enable-static=yes --enable-shared=no
-  make
-  make install
-#
- # c-ares build
-  cd ..
-  $DOWNLOADER $C_ARES
-  tar zxvf c-ares-1.34.4.tar.gz
-  cd c-ares-1.34.4/
-  PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ LD_LIBRARY_PATH=$PREFIX/lib/ CC=$HOST-gcc CXX=$HOST-g++ ./configure --host=$HOST --build=`dpkg-architecture -qDEB_BUILD_GNU_TYPE` --prefix=$PREFIX --enable-static --disable-shared
-  make
-  make install
-#
- # Openssl build
-  cd ..
-  $DOWNLOADER $OPENSSL
-  tar zxvf openssl-3.4.0.tar.gz
-  cd openssl-3.4.0/
-  PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ LD_LIBRARY_PATH=$PREFIX/lib/ CC=$HOST-gcc CXX=$HOST-g++  ./Configure linux-armv4 no-asm $CFLAGS --prefix=$PREFIX shared zlib zlib-dynamic -D_GNU_SOURCE -D_BSD_SOURCE --with-zlib-lib=$LOCAL_DIR/lib --with-zlib-include=$LOCAL_DIR/include
-  make CC=$CC
-  make CC=$CC install INSTALLTOP=$DEST OPENSSLDIR=$DEST/ssl
-#
- # sqlite3
-  cd ..
-  $DOWNLOADER $SQLITE3
-  tar zxvf sqlite-autoconf-3390000.tar.gz
-  cd sqlite-autoconf-3390000/
-  PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ LD_LIBRARY_PATH=$PREFIX/lib/ CC=$HOST-gcc CXX=$HOST-g++ ./configure --host=$HOST --prefix=$PREFIX --enable-static --enable-shared --build=`dpkg-architecture -qDEB_BUILD_GNU_TYPE`
-  make
-  make install
-#
- # libssh2
-  cd ..
-  $DOWNLOADER $SSH2
-  tar zxvf libssh2-1.11.1.tar.gz
-  cd libssh2-1.11.1/
-  rm -rf $PREFIX/lib/pkgconfig/libssh2.pc
-  PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ LD_LIBRARY_PATH=$PREFIX/lib/ CC=$HOST-gcc CXX=$HOST-g++ AR=$HOST-ar RANLIB=$HOST-ranlib ./configure --host=$HOST --without-libgcrypt --with-openssl --without-wincng  --with-libssl-prefix=$PREFIX --prefix=$PREFIX --enable-static --disable-shared LDFLAGS="-L$LOCAL_DIR/openssl/lib" PKG_CONFIG_PATH="$LOCAL_DIR/openssl/lib/pkgconfig" CPPFLAGS="-I$DEST/openssl/include"
-  make
-  make install
-#
- #cleaning
-  cd ..
-  rm -rf c-ares*
-  rm -rf sqlite-autoconf*
-  rm -rf zlib-*
-  rm -rf expat-*
-  rm -rf openssl-*
-  rm -rf libssh2-*
-#
- echo "finished!"
+
+echo "Building zlib-${ZLIB_VER}..."
+$DOWNLOADER $ZLIB_URL
+tar zxvf zlib-${ZLIB_VER}.tar.gz
+cd zlib-${ZLIB_VER}/
+PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ LD_LIBRARY_PATH=$PREFIX/lib/ \
+CC=$CC STRIP=$STRIP RANLIB=$RANLIB CXX=$CXX AR=$AR LD=$LD \
+./configure --prefix=$PREFIX --static
+$MAKE && make install
+cd ..
+
+echo "Building expat-${EXPAT_VER}..."
+$DOWNLOADER $EXPAT_URL
+tar jxvf expat-${EXPAT_VER}.tar.bz2
+cd expat-${EXPAT_VER}/
+PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ LD_LIBRARY_PATH=$PREFIX/lib/ \
+CC=$CC CXX=$CXX \
+./configure --host=$HOST --build=`dpkg-architecture -qDEB_BUILD_GNU_TYPE` --prefix=$PREFIX --enable-static=yes --enable-shared=no
+$MAKE && make install
+cd ..
+
+echo "Building c-ares-${C_ARES_VER}..."
+$DOWNLOADER $C_ARE_URL
+tar zxvf c-ares-${C_ARES_VER}.tar.gz
+cd c-ares-${C_ARES_VER}/
+PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ LD_LIBRARY_PATH=$PREFIX/lib/ \
+CC=$CC CXX=$CXX \
+./configure --host=$HOST --build=`dpkg-architecture -qDEB_BUILD_GNU_TYPE` --prefix=$PREFIX --enable-static --disable-shared
+$MAKE && make install
+cd ..
+
+echo "Building OpenSSL-${OPENSSL_VER}..."
+$DOWNLOADER $OPENSSL_URL
+tar zxvf openssl-${OPENSSL_VER}.tar.gz
+cd openssl-${OPENSSL_VER}/
+# 关键修复：使用 no-module 将 provider 集成进静态库，enable-legacy 开启支持
+PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ LD_LIBRARY_PATH=$PREFIX/lib/ \
+CC=$CC CXX=$CXX \
+./Configure linux-aarch64 no-shared no-module no-asm enable-legacy $CFLAGS --prefix=$PREFIX zlib -D_GNU_SOURCE -D_BSD_SOURCE --with-zlib-lib=$LOCAL_DIR/lib --with-zlib-include=$LOCAL_DIR/include
+$MAKE CC=$CC && make CC=$CC install
+cd ..
+
+echo "Building sqlite-autoconf-${SQLITE_VER}..."
+$DOWNLOADER $SQLITE3_URL
+tar zxvf sqlite-autoconf-${SQLITE_VER}.tar.gz
+cd sqlite-autoconf-${SQLITE_VER}/
+PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ LD_LIBRARY_PATH=$PREFIX/lib/ \
+CC=$CC CXX=$CXX \
+./configure --host=$HOST --prefix=$PREFIX --enable-static --enable-shared --build=`dpkg-architecture -qDEB_BUILD_GNU_TYPE`
+$MAKE && make install
+cd ..
+
+echo "Building libssh2-${LIBSSH2_VER}..."
+$DOWNLOADER $SSH2_URL
+tar zxvf libssh2-${LIBSSH2_VER}.tar.gz
+cd libssh2-${LIBSSH2_VER}/
+rm -rf $PREFIX/lib/pkgconfig/libssh2.pc
+PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ LD_LIBRARY_PATH=$PREFIX/lib/ \
+CC=$CC CXX=$CXX AR=$AR RANLIB=$RANLIB \
+./configure --host=$HOST --without-libgcrypt --with-openssl --without-wincng \
+--with-libssl-prefix=$PREFIX --prefix=$PREFIX --enable-static --disable-shared \
+LDFLAGS="-L$LOCAL_DIR/lib" PKG_CONFIG_PATH="$LOCAL_DIR/lib/pkgconfig" CPPFLAGS="-I$DEST/include"
+$MAKE && make install
+cd ..
+
+# 清理
+echo "Cleaning up..."
+rm -rf c-ares* sqlite-autoconf* zlib-* expat-* openssl-* libssh2-*
+
+echo "All libraries version ${ZLIB_VER}, ${OPENSSL_VER}, ${EXPAT_VER}, ${SQLITE_VER}, ${C_ARES_VER}, ${LIBSSH2_VER} finished!"
--- make-config.sh
+++ make-config.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
 
-# In this configuration, the following dependent libraries are used:
-#
-# * zlib
-# * c-ares
-# * expat
-# * sqlite3
-# * openSSL
-# * libssh2
-
-## CONFIG ##
-ARCH="armhf"
+# ==========================================================
+# 统一环境配置 (需与 libs.sh 保持一致)
+# ==========================================================
+ARCH="aarch64"
 HOST="aarch64-linux-gnu"
 PREFIX="/opt/aria2-aarch64"
 LOCAL_DIR="/opt/aria2-aarch64/build_libs"
 
 TOOL_BIN_DIR="/opt/aria2-aarch64/tools/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin"
-PATH=${TOOL_BIN_DIR}:$PATH
+export PATH=${TOOL_BIN_DIR}:$PATH
 
 CFLAGS="-march=armv8-a -mtune=cortex-a53"
-DEST="/opt/aria2-aarch64/build_libs"
+CXXFLAGS=$CFLAGS
+LDFLAGS="-L$LOCAL_DIR/lib"
+CPPFLAGS="-I$LOCAL_DIR/include"
+
 CC=$HOST-gcc
 CXX=$HOST-g++
-LDFLAGS="-L$DEST/lib"
-CPPFLAGS="-I$DEST/include"
-CXXFLAGS=$CFLAGS
+
 MAKE="make -j`nproc`"
-CONFIGURE="./configure --prefix=${LOCAL_DIR} --host=$HOST"
+
+# ==========================================================
+# aria2 主程序配置
+# ==========================================================
+echo "Configuring aria2 for $HOST..."
 
 ./configure \
     --host=$HOST \
@@ -38,10 +36,11 @@ CONFIGURE="./configure --prefix=${LOCAL_DIR} --host=$HOST"
     --without-libxml2 \
     --with-libz --with-libz-prefix=${LOCAL_DIR} \
     --with-libexpat --with-libexpat-prefix=${LOCAL_DIR} \
-    --with-slite3 --with-sqlite3-prefix=${LOCAL_DIR} \
+    --with-sqlite3 --with-sqlite3-prefix=${LOCAL_DIR} \
     --with-libcares --with-libcares-prefix=${LOCAL_DIR} \
-    LDFLAGS="-L$LOCAL_DIR/lib" \
+    LDFLAGS="$LDFLAGS" \
     PKG_CONFIG_PATH="$LOCAL_DIR/lib/pkgconfig" \
     ARIA2_STATIC=yes
 
+echo "Building aria2..."
 $MAKE
